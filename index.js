fb1ec25de32926f356796985ebef4a68bd75e3a9
rohma01 <rohithmadhuker@gmail.com>
2023-11-22 02:26:31 -0500

    draft stuff and some other stuff

--- index.js
+++ index.js
@@ -2,12 +2,40 @@ const express = require('express');
 const ejs = require("ejs");
 const PORT = 3000;
 const app=express();
+const bodyParser = require('body-parser');
+const mongoose=require('mongoose');
+const User=require('./models/users');
+mongoose.connect("mongodb://localhost:27017/FootballMadness", {useNewUrlParser: true, useUnifiedTopology: true})
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/",(req,res) => {
     res.render("index");
 });
 
+app.get("/signup", (req,res) => {
+    res.render("signup")
+});
+
+app.get("/login", (req,res) => {
+    res.render("login");
+});
+
+app.post("homepage", (req,res) => {
+    const userName=req.body.userName;
+    const password=req.body.password;
+
+    const newUser = new User({
+        userName: userName,
+        password: password
+    })
+
+    newUser.save((err) => {
+        err ? console.log(err) : res.send("Succesfully Created User");
+    });
+})
 app.listen(PORT,() => console.log("Server started on port 3000"));
--- models/users.js
+++ models/users.js
@@ -1,2 +1,8 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb:")
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model("User", userSchema);
+module.exports=User;
